513cbebe6fa9c1d40f94f8ad6cdd5afef2c1242a
zdh431079799 <57171915+zdh431079799@users.noreply.github.com>
2022-02-18 15:39:38 +0800

    Add files via upload

--- simpleEA.m
+++ simpleEA.m
@@ -27,27 +27,98 @@ fitness_gen=[]; % record the best fitness so far
 solution_gen=[];% record the best phenotype of each generation
 fitness_pop=[];% record the best fitness in current population 
 %% Below starting your code
-
+nbPop = 4;
+lb = 0;
+ub = 31;
+lenGene = 5;
 % Initialise a population
 %% TODO
-
+pop = randi([lb,ub],nbPop,1);
+genotype = dec2bin(pop,lenGene);
 
 % Evaluate the initial population
 %% TODO
+fitness = objective(pop);
+[m,idx]=max(fitness);
+if m > bestSoFarFit
+    bestSoFarFit = m;
+    bestSoFarSolution = pop(idx);
+end
+fitness_gen = [fitness_gen,bestSoFarFit];
+solution_gen = [solution_gen,bestSoFarSolution];
+fitness_pop = [fitness_pop,m];
 
+nbEval = nbEval + nbPop;
+nbGen =  nbGen +1;
 % Start the loop
-while (nbEval<T) 
+while (nbEval<T)
 % Reproduction (selection, crossver)
 %% TODO
+selectProb = fitness./sum(fitness);
+newpop_genotype = [];
+for i=1:nbPop/2
+    r1 = rand();
+    r2 = rand();
+    p1_idx = 0;
+    p2_idx = 0;
+    s = 0;
+    for index = 1:nbPop
+        s = s+selectProb(index);
+        if p1_idx==0 && r1<s
+            p1_idx = index;
+        end
+        if p2_idx==0 && r2<s
+            p2_idx = index;
+        end
+    end
+    crosspoint = randi(lenGene-1);
+    c1 = [genotype(p1_idx,1:crosspoint),genotype(p2_idx,crosspoint+1:end)];
+    c2 = [genotype(p2_idx,1:crosspoint),genotype(p1_idx,crosspoint+1:end)];
+    newpop_genotype = [newpop_genotype;c1;c2];
+end
 
 % Mutation
 %% TODO
+mutationProb = 1/lenGene;
+for i=1:nbPop
+    for j=1:lenGene
+        if rand()<mutationProb
+            if newpop_genotype(i,j) == '0'
+                newpop_genotype(i,j) = '1';
+            else
+                newpop_genotype(i,j) = '0';
+            end
+        end
+    end
+end
+newpop = bin2dec(newpop_genotype);
 
+pop = newpop;
+genotype = newpop_genotype;
+
+fitness = objective(pop);
+[m,idx]=max(fitness);
+if m > bestSoFarFit
+    bestSoFarFit = m;
+    bestSoFarSolution = pop(idx);
+end
+fitness_gen = [fitness_gen,bestSoFarFit];
+solution_gen = [solution_gen,bestSoFarSolution];
+fitness_pop = [fitness_pop,m];
+
+nbEval = nbEval + nbPop;
+nbGen =  nbGen +1;
+
+end
 
 bestSoFarFit
 bestSoFarSolution
 
+figure,plot(1:nbGen,fitness_gen,'b') 
+title('Fitness\_Gen')
 
+figure,plot(1:nbGen,solution_gen,'b') 
+title('Solution\_Gen')
 
-
-
+figure,plot(1:nbGen,fitness_pop,'b') 
+title('Fitness\_Pop')
